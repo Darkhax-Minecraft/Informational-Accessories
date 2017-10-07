f760f8b8aab5de1a46552f8c78b07c56f72afe37
Tyler Hancock <darklime@live.ca>
2017-10-07 12:23:10 -0600

    Enabled auto registry.

--- src/main/java/net/darkhax/infoaccessories/InfoAccessories.java
+++ src/main/java/net/darkhax/infoaccessories/InfoAccessories.java
@@ -5,22 +5,17 @@ import net.darkhax.bookshelf.registry.RegistryHelper;
 import net.darkhax.infoaccessories.items.ItemInfoAccessory;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = "infoaccessories", name = "Info Accessories", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.1.449,);", certificateFingerprint = "@FINGERPRINT@")
+@Mod(modid = "infoaccessories", name = "Info Accessories", version = "@VERSION@", dependencies = "required-after:bookshelf@[2.2.462,);", certificateFingerprint = "@FINGERPRINT@")
 public class InfoAccessories {
     
     public static final LoggingHelper LOG = new LoggingHelper("Info Accessories");
-    public static final RegistryHelper REGISTRY = new RegistryHelper("infoaccessories").setTab(CreativeTabs.MISC);
+    public static final RegistryHelper REGISTRY = new RegistryHelper("infoaccessories").enableAutoRegistration().setTab(CreativeTabs.MISC);
     public static Item infoItem;
     
     @EventHandler
@@ -29,25 +24,6 @@ public class InfoAccessories {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new InfoHandler());
         infoItem = REGISTRY.registerItem(new ItemInfoAccessory(), "info_accessory");
-    }
-    
-    @SubscribeEvent
-    public void registerItems (RegistryEvent.Register<Item> event) {
-    
-        for (Item item : REGISTRY.getItems()) {
-            
-            event.getRegistry().register(item);
-        }
-    }
-    
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public void registerModels (ModelRegistryEvent event) {
-    
-        for (Item item : REGISTRY.getItems()) {
-            
-            REGISTRY.registerInventoryModel(item);
-        }
     }
     
     @EventHandler
