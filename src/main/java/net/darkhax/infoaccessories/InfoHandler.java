9de2cc0e13aac9bdcf220993b1f802592173d35a
Tyler Hancock <darklime@live.ca>
2018-01-02 19:13:31 -0700

    Prevent players from cheating info with mods like JEI!

--- src/main/java/net/darkhax/infoaccessories/InfoHandler.java
+++ src/main/java/net/darkhax/infoaccessories/InfoHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.darkhax.bookshelf.util.PlayerUtils;
 import net.darkhax.infoaccessories.info.InfoType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.ai.attributes.AttributeMap;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.IAttribute;
@@ -13,6 +14,7 @@ import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -21,69 +23,78 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class InfoHandler {
-    
+
     @SubscribeEvent(priority = EventPriority.LOWEST)
     @SideOnly(Side.CLIENT)
     public void onDebugScreen (RenderGameOverlayEvent.Text event) {
-    
+
         final Minecraft mc = Minecraft.getMinecraft();
         final EntityPlayer player = mc.player;
         final List<String> left = event.getLeft();
         final List<String> right = event.getRight();
-        
-        boolean show = ConfigurationHandler.invertF3 ? mc.gameSettings.showDebugInfo : !mc.gameSettings.showDebugInfo;
-        		
+
+        final boolean show = ConfigurationHandler.invertF3 ? mc.gameSettings.showDebugInfo : !mc.gameSettings.showDebugInfo;
+
         // Exit if not debug mode
         if (show) {
-            
+
             return;
         }
-        
+
         // Removes existing info
-        // TODO add config to keep entries. 
+        // TODO add config to keep entries.
         left.clear();
         right.clear();
-        
+
         // Adds info from the onfo types
-        for (InfoType type : InfoType.values()) {
-            
+        for (final InfoType type : InfoType.values()) {
+
             if (type.canPlayerSee(player)) {
-                
+
                 type.getInfo(player.world, player, left, right);
             }
         }
     }
-    
+
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public void onTooltipEvent (ItemTooltipEvent event) {
-    
+
         if (event.getEntityPlayer() == null) {
-            
+
             return;
         }
-        
+
         // Adds info from the info types
-        for (InfoType type : InfoType.values()) {
-            
+        for (final InfoType type : InfoType.values()) {
+
             if (type.isValidItem(event.getItemStack())) {
-                
-                type.getInfo(event.getEntityPlayer().world, event.getEntityPlayer(), event.getToolTip(), event.getFlags().isAdvanced() ? event.getToolTip() : new ArrayList<>());
+
+                if (!type.canPlayerSee(event.getEntityPlayer())) {
+
+                    event.getToolTip().add(TextFormatting.RED + I18n.format("tooltip.infoaccessories.lack.item"));
+                    break;
+                }
+
+                else {
+
+                    type.getInfo(event.getEntityPlayer().world, event.getEntityPlayer(), event.getToolTip(), event.getFlags().isAdvanced() ? event.getToolTip() : new ArrayList<>());
+                }
             }
         }
     }
-    
+
     public static double getStackAttribute (ItemStack stack, EntityEquipmentSlot slot, IAttribute attribute) {
-    
+
         final IAttributeInstance instance = new AttributeMap().registerAttribute(attribute);
-        
-        EntityPlayer p = PlayerUtils.getClientPlayer();
+
+        final EntityPlayer p = PlayerUtils.getClientPlayer();
         instance.setBaseValue(p.getEntityAttribute(attribute).getBaseValue());
-        for (AttributeModifier modifier : stack.getAttributeModifiers(slot).get(attribute.getName())) {
-            
+        for (final AttributeModifier modifier : stack.getAttributeModifiers(slot).get(attribute.getName())) {
+
             instance.applyModifier(modifier);
         }
-        
+
         return instance.getAttributeValue();
     }
 }
